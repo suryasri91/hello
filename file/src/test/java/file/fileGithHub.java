7c203b255ab6f4026b1f25c3054e8c11ce84eda7
suryasri91 <mccheerssurya007@gmail.com>
2024-11-25 19:48:08 +0530

    first push

---
+++ file/src/test/java/file/fileGithHub.java
@@ -0,0 +1,11 @@
+package file;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class fileGithHub {
+	@Test
+	public void demo() {
+		Reporter.log("welcome git");
+	}
+}
